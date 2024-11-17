377a63f423244beed28d3f4004a039c5fd383f02
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-11-17 14:33:45 +0900

    refactor: 재고 부족 예외 구현 수정

--- src/main/java/store/validator/Validator.java
+++ src/main/java/store/validator/Validator.java
@@ -5,9 +5,16 @@ import store.model.ProductManager;
 
 public class Validator {
     public static void validateQuantity(String productName, int quantity) {
-        Product product = ProductManager.findWithoutPromotionProductByName(productName);
-        Product product1 = ProductManager.findPromotionProductByName(productName);
-        int availableStock = product.getQuantity() + product1.getQuantity();
+        Product promotionProductQuantity = ProductManager.findWithoutPromotionProductByName(productName);
+        Product nonPromotionProductQuantity = ProductManager.findPromotionProductByName(productName);
+        int availableStock = 0;
+
+        if (promotionProductQuantity != null) {
+            availableStock += promotionProductQuantity.getQuantity();
+        }
+        if (nonPromotionProductQuantity != null) {
+            availableStock += nonPromotionProductQuantity.getQuantity();
+        }
 
         if (quantity > availableStock) {
             throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
